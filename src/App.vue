9a2f93679c29fa5f474a27e10bb553419ad436a5
Jakub Janik <jakubjanik@vp.pl>
2020-07-10 08:11:43 +0200

    Render JobFilters component and edit background

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,37 @@
 <template>
   <div id="app">
-    <header></header>
-    <main>
-      <Job
-        v-for="job in jobs"
-        :job="job"
-        :key="job.id"
-        @filterJobs="filterJobs"
-      />
-    </main>
+    <JobFilters v-show="filters.length" :filters="filters" />
+    <Job
+      v-for="job in jobs"
+      :job="job"
+      :key="job.id"
+      @filterJobs="filterJobs"
+    />
   </div>
 </template>
 
 <script>
 import allJobs from "@/jobs.json";
 import Job from "@/components/Job";
+import JobFilters from "@/components/JobFilters";
 
 export default {
   name: "App",
   components: {
-    Job
+    Job,
+    JobFilters
   },
   data() {
     return {
-      jobs: allJobs
+      jobs: allJobs,
+      filters: []
     };
   },
   methods: {
     filterJobs({ field, value }) {
       this.jobs = this.jobs.filter(job => job[field].includes(value));
+
+      this.filters.push(value);
     }
   }
 };
@@ -41,25 +44,27 @@ body {
   margin: 0;
   font-size: 15px;
   font-family: "Spartan", sans-serif;
+  background: url("assets/bg-header-desktop.svg") hsl(180, 52%, 96%) no-repeat;
+  background-size: 100% 156px;
+  padding-top: 156px;
 }
 
-header {
-  height: 156px;
-  background: url("assets/bg-header-desktop.svg") hsl(180, 29%, 50%) no-repeat;
-  background-size: cover;
-}
-
-main {
+#app {
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 3em 2em;
-  background: hsl(180, 52%, 96%);
+  padding: 4em 2em;
 }
 
-@media all and (max-width: 1000px) {
+@media (max-width: 1000px) {
   main {
     padding: 2em 1.5em;
+  }
+}
+
+@media (max-width: 800px) {
+  body {
+    background-image: url("assets/bg-header-mobile.svg");
   }
 }
 </style>
