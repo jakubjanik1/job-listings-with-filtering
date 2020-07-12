ce5e9dee1005c98937483cbe4b1519be39a89145
Jakub Janik <jakubjanik@vp.pl>
2020-07-12 06:06:21 +0200

    Add remove option to JobFilters

--- src/components/JobFilters.vue
+++ src/components/JobFilters.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="job-filters">
-    <div class="job-filters__filter" v-for="filter in filters" :key="filter">
+    <div class="job-filters__filter" v-for="filter in value" :key="filter">
       {{ filter }}
+      <div class="job-filters__remove" @click="remove(filter)">
+        <img src="@/assets/icon-remove.svg" alt="remove" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ["filters"]
+  props: ["value"],
+  methods: {
+    remove(filter) {
+      const index = this.value.indexOf(filter);
+      this.value.splice(index, 1);
+
+      this.$emit("input", this.value);
+    }
+  }
 };
 </script>
 
@@ -32,11 +43,28 @@ export default {
     font-weight: 700;
     font-size: 0.9em;
     height: 2.5em;
-    padding: 0 0.75em;
+    padding-left: 0.75em;
     line-height: 2.8em;
+    display: flex;
 
     &:not(:first-child) {
       margin-left: 1.5em;
+    }
+  }
+
+  &__remove {
+    padding: 0 0.75em;
+    background-color: hsl(180, 29%, 50%);
+    color: #fff;
+    border-radius: 0 0.5em 0.5em 0;
+    margin-left: 0.75em;
+    display: flex;
+    align-items: center;
+    transition: background-color 300ms;
+
+    &:hover {
+      background-color: hsl(180, 14%, 20%);
+      cursor: pointer;
     }
   }
 }
